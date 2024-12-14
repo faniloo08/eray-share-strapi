b2f70dd0ac5b7f5c5d25e1330940c69ca45c0ac0
Fanilo Rabemanantsoa <93961550+faniloo08@users.noreply.github.com>
2024-12-14 11:36:05 +0300

    Add files via upload

---
+++ src/api/payes-formateur/controllers/payes-formateur.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * payes-formateur controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::payes-formateur.payes-formateur');
