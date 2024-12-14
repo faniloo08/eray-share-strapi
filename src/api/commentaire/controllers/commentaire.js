96a7cfd82874da1334f5cf8b5a164162d9d30c56
Fanilo Rabemanantsoa <93961550+faniloo08@users.noreply.github.com>
2024-12-14 11:04:54 +0300

    Add files via upload

---
+++ src/api/commentaire/controllers/commentaire.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * commentaire controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::commentaire.commentaire');
