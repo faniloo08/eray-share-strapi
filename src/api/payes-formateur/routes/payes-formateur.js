d323d4158963b18f1caa87f02d475d82bb67c118
Fanilo Rabemanantsoa <93961550+faniloo08@users.noreply.github.com>
2024-12-14 11:37:02 +0300

    Add files via upload

---
+++ src/api/payes-formateur/routes/payes-formateur.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * payes-formateur router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::payes-formateur.payes-formateur');
