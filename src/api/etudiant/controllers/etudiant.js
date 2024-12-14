2ebe66dc0fa42099402cc7beaa9db1a6d0f15420
Fanilo Rabemanantsoa <93961550+faniloo08@users.noreply.github.com>
2024-12-14 11:14:45 +0300

    Add files via upload

---
+++ src/api/etudiant/controllers/etudiant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * etudiant controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::etudiant.etudiant');
